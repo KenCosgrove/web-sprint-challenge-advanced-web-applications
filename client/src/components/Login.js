2e58adbcbc5d89fb750330fe3eb16be7053a4db8
AJ Gambino <aj.gambino7@gmail.com>
2020-09-18 11:17:32 -0400

    setup

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -5,8 +5,7 @@ const Login = () => {
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      
     </>
   );
 };
